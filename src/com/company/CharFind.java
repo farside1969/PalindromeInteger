2a9b6583f8b9f190259f535bacd3133e0c0fa635
farside1969 <farside1969@hotmail.com>
2019-08-23 10:59:04 -0500

    added charFind

---
+++ src/com/company/CharFind.java
@@ -0,0 +1,18 @@
+package com.company;
+
+// class
+public class CharFind {
+
+// method
+    public static void main(String[] args) {
+
+// declare
+        String text = "find your foo";
+
+// use charAt() to find character by index location
+        char z = text.charAt(8);
+
+// print results
+        System.out.println(z);
+    }
+}
